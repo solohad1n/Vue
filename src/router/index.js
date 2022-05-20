e96863fed5b6b3b6241f7a773119b291e990b9f1
solohad1n <fox663399@gmail.com>
2022-05-20 19:22:02 +0600

    Created a Cart page

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Cart from '../views/Cart.vue'
+
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  }
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart,
+  },
 ]
 
 const router = createRouter({
